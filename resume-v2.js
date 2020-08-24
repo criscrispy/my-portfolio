e4db1adecec6ccb2b8cbde7964684545ff472885
criscrispy <jlcaddy4@gmail.com>
2020-08-24 23:10:00 +0300

    add animation with AOS lib & emojis to typed intro

--- resume-v2.js
+++ resume-v2.js
@@ -2,30 +2,32 @@
 //  Typed Js
 // ========================================================================= //
 
-$(document).ready(function () {
-    'use strict';
-    var typed = $(".typed");
+$(document).ready(function() {
+	'use strict';
+	var typed = $('.typed');
 
-    $(function () {
-        typed.typed({
-            strings: ["My name is Christian Addy",
-                "but you can just call me Chris.",
-                "Having worked as Customer support engineer",
-                "and an IT administrator,",
-                "I now aspire to be a Frontend developer.",
-                "So one day, this text will include...",
-                "Chris - the fronted developer :)",
-                "But for now the journey continues...",
-                "Ok, ...back to intro message.",
-                "I love to build projects",
-                "and explore different technologies.",
-                "Check out these projects and more about me below.",
-                "Thanks for stopping by",
-                "and have a wonderful day :) ",],
-            typeSpeed: 50,
-            loop: false,
-        });
-    });
+	$(function() {
+		typed.typed({
+			strings    : [
+				'My name is Christian Addy ^1000',
+				'but you can just call me Chris.',
+				'Having worked as Customer support engineer',
+				'and an IT administrator,',
+				'I now aspire to be a Frontend developer.',
+				'So one day, this text will include ^1500...drum roll 🥁',
+				'Chris - the frontend developer 🙂',
+				'Ok, ...back to intro message.',
+				'I love to build projects',
+				'and explore different technologies.',
+				'Check out these projects and more about me below.',
+				'Thanks for stopping by',
+				'and have a wonderful day 🙂 '
+			],
+			typeSpeed  : 50,
+			loop       : false,
+			startDelay : 200
+		});
+	});
 });
 
 // ================================================== //
@@ -34,16 +36,16 @@ $(document).ready(function () {
 // Spinner
 // ================================================== //
 
-$(document).ready(function () {
-    //Preloader
-    preloaderFadeOutTime = 1500;
+$(document).ready(function() {
+	//Preloader
+	preloaderFadeOutTime = 1500;
 
-    function hidePreloader() {
-        var preloader = $('.spinner-wrapper');
+	function hidePreloader() {
+		var preloader = $('.spinner-wrapper');
 
-        preloader.fadeOut(preloaderFadeOutTime);
-    }
-    hidePreloader();
+		preloader.fadeOut(preloaderFadeOutTime);
+	}
+	hidePreloader();
 });
 
 // $(document).ready(function () {
@@ -57,11 +59,7 @@ $(document).ready(function () {
 //     showMainProfileImage();
 // });
 
-
 // ================================================== //
-
-
-
 
 // ================================================== //
 // Show description of crossover products on small and extra small screens
@@ -77,9 +75,16 @@ $(document).ready(function () {
 // });
 
 // Example 2
-$(function () {
-    $('[data-toggle="crossover-product-description-sm-xs"]').tooltip()
-})
-
+$(function() {
+	$('[data-toggle="crossover-product-description-sm-xs"]').tooltip();
+});
 
 // ================================================== //
+
+AOS.init({
+	delay    : 200, // values from 0 to 3000, with step 50ms
+	duration : 1200, // values from 0 to 3000, with step 50ms
+	easing   : 'ease', // default easing for AOS animations
+	once     : true, // whether animation should happen only once - while scrolling down
+	mirror   : false // whether elements should animate out while scrolling past them
+});
