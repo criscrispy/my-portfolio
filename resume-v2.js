b3abafdd27ad954ae9745bb9c3082014cdec3e8c
Christian Addy <jlcaddy4@gmail.com>
2022-01-22 23:35:41 +0200

    firt commit from new PC

--- resume-v2.js
+++ resume-v2.js
@@ -85,3 +85,6 @@ AOS.init({
 	once: true, // whether animation should happen only once - while scrolling down
 	mirror: false // whether elements should animate out while scrolling past them
 });
+
+
+// Testing from new legion PC
